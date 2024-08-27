35aad2c71595457b129d6552741d4cdc4258e2e9
igor.bondezam <igorbfraca@gmail.com>
2024-08-27 00:22:40 -0300

    Melhorando visualização da listagem, entendo comportamentos de classes internas e criando um header para o app

--- lib/components/my-app.dart
+++ lib/components/my-app.dart
@@ -1,34 +1,31 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:igor_bancaria/components/listagem-separada.dart';
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  final List<String> dados = ["Hello", "World", "Olá", "Mundo"];
-  final List<int> colorCodes = <int>[600, 500, 400, 100];
-
+  List<String> dados = ["Pix", "Crédito", "Débito", "Empréstimos"];
 
   @override
-    Widget build(BuildContext context){
-        return MaterialApp(
-          title: "Aplicação Bancária",
-          theme: ThemeData(
-            primaryColor: Colors.teal
-          ),
-          home: Card(
-            child: ListView.separated(
-              padding: const EdgeInsets.all(8),
-              itemCount: dados.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 50,
-                  color: Colors.amber[colorCodes[index]],
-                  child: Center(child: Text(dados[index])),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) => const Divider(),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Aplicação Bancária",
+        theme: ThemeData(primaryColor: Colors.teal),
+        home: Scaffold(
+          body: Column(children: [
+            // Header
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16),
+              color: Colors.teal,
+              child: const Text(
+                'IgorBank',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-        );
-    }
+            Expanded(child: Card(child: ListagemSeparada(dados: dados))),
+          ]),
+        ));
+  }
 }
